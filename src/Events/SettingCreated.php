4aeb24ba995bcd793f9f5f950bed4761ccf5fd5f
Orkhan Ahmadov <hi@orkhan.dev>
2022-04-25 21:01:26 +0200

    WIP

---
+++ src/Events/SettingCreated.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orkhanahmadov\ModelSettings\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Orkhanahmadov\ModelSettings\Models\Setting;
+
+class SettingCreated
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    public function __construct(public Setting $setting)
+    {
+    }
+}
--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -7,6 +7,9 @@ namespace Orkhanahmadov\ModelSettings\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Orkhanahmadov\ModelSettings\Enums\Type;
+use Orkhanahmadov\ModelSettings\Events\SettingCreated;
+use Orkhanahmadov\ModelSettings\Events\SettingDeleted;
+use Orkhanahmadov\ModelSettings\Events\SettingUpdated;
 
 class Setting extends Model
 {
@@ -23,6 +26,13 @@ class Setting extends Model
     protected $casts = [
         'type' => Type::class,
     ];
+
+    protected static function booted()
+    {
+        static::created(fn (Setting $setting) => event(new SettingCreated($setting)));
+        static::updated(fn (Setting $setting) => event(new SettingUpdated($setting)));
+        static::deleted(fn (Setting $setting) => event(new SettingDeleted($setting)));
+    }
 
     public function getCasts()
     {
