cc2010a783962640557e3cc10b04d72038b44d55
Orkhan Ahmadov <hi@orkhan.dev>
2022-04-25 18:46:35 +0200

    WIP

---
+++ tests/Concerns/HasSettingsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orkhanahmadov\ModelSettings\Tests\Concerns;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Orkhanahmadov\ModelSettings\Tests\TestCase;
+
+class HasSettingsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
